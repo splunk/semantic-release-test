61c8507ec1af88cf5d5691f99eff2703ffae8e45
mkolasinski-splunk <105011638+mkolasinski-splunk@users.noreply.github.com>
2022-12-16 14:47:17 +0100

    fix:add debug prints

--- semantic.sh
+++ semantic.sh
@@ -1,4 +1,6 @@
+echo "Started semantic.sh"
 npm install -D @semantic-release/changelog @semantic-release/git @semantic-release/exec
 export NEXT_VERSION=$(npx semantic-release --dry-run | grep 'The next release version is' | sed -E 's/.* ([[:digit:].]+)$/\1/')
 echo version="$NEXT_VERSION" >> $GITHUB_OUTPUT
+echo "printing next version value"
 echo "$NEXT_VERSION"
