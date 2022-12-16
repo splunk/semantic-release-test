2a30aec92891314d572d5630b5af29f3857e3a80
mkolasinski-splunk <mkolasinski@splunk.com>
2022-12-16 13:01:37 +0100

    feat: add action files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
+npm install -D @semantic-release/changelog @semantic-release/git @semantic-release/exec
+export NEXT_VERSION=$(npx semantic-release --dry-run | grep 'The next release version is' | sed -E 's/.* ([[:digit:].]+)$/\1/')
+echo version="$NEXT_VERSION" >> $GITHUB_OUTPUT
